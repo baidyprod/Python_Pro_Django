937c7d784dc1de6250582dfe5c0960e056aad209
baidyprod <dima220920041@gmail.com>
2023-03-14 00:46:07 +0200

    Commit 4 - Management commands fix. New models.

--- catalog/management/commands/create_users.py
+++ catalog/management/commands/create_users.py
@@ -1,20 +1,20 @@
-from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.core.management.base import BaseCommand
 
 from faker import Faker
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
     command_help = 'Generates bulk users'
 
     def add_arguments(self, parser):
-        parser.add_argument('count', type=int, help='The number of users to generate')
+        parser.add_argument('count', type=int, choices=range(1, 11), help='The number of users to generate')
 
     def handle(self, *args, **options):
         count = options['count']
-
-        if count < 1 or count > 10:
-            raise CommandError("Count must be from 1 to 10")
 
         faker = Faker()
         users = []
@@ -22,8 +22,7 @@ class Command(BaseCommand):
             username = faker.user_name()
             email = faker.ascii_email()
             password = faker.password()
-            user = User(username=username, email=email)
-            user.set_password(password)
+            user = User(username=username, email=email, password=make_password(password))
             users.append(user)
 
         User.objects.bulk_create(users)
